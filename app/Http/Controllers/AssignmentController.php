14fd52b6c361d5039e790a498ef0f50b097a4045
hoangph3 <hoangph3@pop-os.localdomain>
2020-10-03 20:34:09 +0700

    upload task and mkdir folder solution

--- app/Http/Controllers/AssignmentController.php
+++ app/Http/Controllers/AssignmentController.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Support\Facades\DB;
-
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 
 class AssignmentController extends Controller
@@ -17,8 +17,21 @@ class AssignmentController extends Controller
         if ($request->hasFile('fileUpload')) 
         {
             $file = $request->fileUpload;
+            $dir = 'uploads/assignment/' . $file->getClientOriginalName();
 
-            $file->move('uploads/assignment/', $file->getClientOriginalName());
+            if (! File::exists($dir))
+            {
+                $file->move('uploads/assignment/', $file->getClientOriginalName());
+                
+                $folder = 'uploads/solution/' . $file->getClientOriginalName();
+                File::makeDirectory($folder);
+                
+                return view('assignments.index');
+            }
+            else
+            {
+                echo "<script>alert('File Exist!');</script>";
+            }
         }
     }
 }
