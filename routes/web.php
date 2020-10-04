ad13df2ad386311ad5bdb87a4ad9b70f5216a901
hoangph3 <hoangph3@pop-os.localdomain>
2020-10-04 14:36:37 +0700

    out source

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function(){
+    return view('login');
+});
 //Route Login - LogOut
 Route::get('login', function(){
     return view('login');
